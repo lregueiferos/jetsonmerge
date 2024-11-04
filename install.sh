b2a2800833e5da85936c7bc072d783285da2961f
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2024-11-04 11:44:17 -0500

    updates system before installation

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
+set -e
+sudo apt update
+sudo apt upgrade -y
+git submodule update --init
 cd SubsystemsPi
 bash install.sh
 cd .. 
 cd SecondaryPi
 bash install.sh
+echo "Installation is Complete"
+sudo shutdown -r now
